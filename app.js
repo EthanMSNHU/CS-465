95d3715e044c847e6566e2ff645b432dc52bff57
Ethan Mills <Ethanmill14@yahoo.com>
2025-07-13 10:49:52 -0400

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('hbs');
+var handlebars = require('hbs');
+var logger = require('dev');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -10,10 +11,11 @@ var usersRouter = require('./app_server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlbars partials (https://www.npmjs.com/package/hbs)
-logger.handlebars.registerPartial(__dirname + '/app_server/views/partials');
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -24,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
